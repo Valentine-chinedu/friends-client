1e3fb031cdb9d365799472210958470cc7bb0795
Valentine <okosival@gmail.com>
2022-09-29 15:45:25 +0100

    axios setup completed

---
+++ src/utils/config.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://friends-server-qd6e.vercel.app/';
---
+++ src/utils/fetchData.js
@@ -0,0 +1,36 @@
+import myAxios from './axios';
+
+export const getDataAPI = async (url, token) => {
+	const res = await myAxios.get(`/api/${url}`, {
+		headers: { Authorization: token },
+	});
+	return res;
+};
+
+export const postDataAPI = async (url, post, token) => {
+	const res = await myAxios.post(`/api/${url}`, post, {
+		headers: { Authorization: token },
+	});
+	return res;
+};
+
+export const putDataAPI = async (url, post, token) => {
+	const res = await myAxios.put(`/api/${url}`, post, {
+		headers: { Authorization: token },
+	});
+	return res;
+};
+
+export const patchDataAPI = async (url, post, token) => {
+	const res = await myAxios.patch(`/api/${url}`, post, {
+		headers: { Authorization: token },
+	});
+	return res;
+};
+
+export const deleteDataAPI = async (url, token) => {
+	const res = await myAxios.delete(`/api/${url}`, {
+		headers: { Authorization: token },
+	});
+	return res;
+};
