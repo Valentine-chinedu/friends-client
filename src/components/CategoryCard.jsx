6d52cbfdce99045c94db74a9b4f4f76f3dc14100
Valentine <okosival@gmail.com>
2022-10-01 13:31:44 +0100

    feat: theme added

---
+++ src/components/CategoryCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardBody from './category_card/CardBody';
+
+const CategroyCard = ({ category, theme }) => {
+	return (
+		<div className='card my-2 '>
+			<CardBody category={category} theme={theme} />
+		</div>
+	);
+};
+
+export default CategroyCard;
