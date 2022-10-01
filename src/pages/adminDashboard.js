201ece4bdf16816b57cdc4fc43cc89df38f1e80b
Valentine <okosival@gmail.com>
2022-10-01 19:41:58 +0100

    imported dashboardlayout

---
+++ src/pages/adminDashboard.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import DashboardLayout from '../components/layouts/dashboard-layout';
+// import Sidebar from './../components/adminDashboard/sidebar/Sidebar';
+
+const AdminDashboard = () => {
+	return <DashboardLayout></DashboardLayout>;
+};
+
+export default AdminDashboard;
