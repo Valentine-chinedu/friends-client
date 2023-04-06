a5855d86c5d0b0a9986cd4bb1f1031f2cdc91dd7
Valentine-chinedu <okosival@gmail.com>
2023-04-06 21:29:10 +0100

    created a separate node element for modal and backdrop using createPortal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 import { useSelector } from 'react-redux';
 import './modal.css';
 
 const Modal = () => {
-	const modal = useSelector((state) => state.modal);
-	return (
-		<div className={modal.visible ? 'modal visible' : 'modal'}>
+	const { modals } = useSelector((state) => state.modal);
+	const node = modals.map((modal) => (
+		<div className='modal' key={modal.id}>
 			<p>{modal.msg}</p>
 		</div>
-	);
+	));
+	return createPortal(node, document.getElementById('modals'));
 };
 
 export default Modal;
